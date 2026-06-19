4adbf4b0e59e3b1f8606ecbb66b3f69ccc225a95
Pedro <molinaripedro76@email.com>
2026-06-19 20:25:09 -0300

    Implementa Usuario e cadastroLivro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -3,4 +3,13 @@ import java.util.ArrayList;
 public class Biblioteca {
     private ArrayList<Livro> livros;
     private ArrayList<Usuario> usuarios;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<Livro>();
+        this.usuarios = new ArrayList<Usuario>();
+    }
+
+    public void cadastrarLivro(Livro livro) {
+        this.livros.add(livro);
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -2,4 +2,14 @@ public class Usuario {
     private int id;
     private String nome;
     private String senha;
+
+    public Usuario(
+            String nome,
+            int id,
+            String senha
+    ) {
+        this.nome = nome;
+        this.id = id;
+        this.senha = senha;
+    }
 }
