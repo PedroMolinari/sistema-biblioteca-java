90c7cafb79de49c07b3141416d578f672d3cfa60
Pedro Henrique Molinari domingues <molinaripedro76@gmail.com>
2026-07-15 17:25:29 -0300

    Remove commented Scanner code
    Removed commented-out Scanner initialization.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
 
-        // Scanner
         Scanner scanner = new Scanner(System.in);
         int opcao = -1;
 
